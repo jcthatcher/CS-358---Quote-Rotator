199ff375a095f36fe9696971947721754e185ce2
Jason Thatcher <jcthatcher@eagles.usi.edu>
2018-01-11 09:54:13 -0600

    Initial - Quote Rotation - Basic Windows App.

--- Hello_1/Form1.Designer.cs
+++ Hello_1/Form1.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDisplay.Location = new System.Drawing.Point(32, 33);
+            this.txtDisplay.Multiline = true;
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.Size = new System.Drawing.Size(414, 99);
+            this.txtDisplay.TabIndex = 1;
+            this.txtDisplay.Text = "Click Button Below to Start";
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(186, 149);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(106, 33);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "Start";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(490, 194);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.txtDisplay);
+            this.Name = "Form1";
+            this.Text = "Quotes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.Button btnNext;
     }
 }
 
--- Hello_1/Form1.cs
+++ Hello_1/Form1.cs
@@ -12,9 +12,39 @@ namespace Hello_1
 {
     public partial class Form1 : Form
     {
+        int tracker;
+        string[] quotes;
+
         public Form1()
         {
+            tracker = 0;            
+            quotes = new string[]{ "Moral indignation is jealousy with a halo.- H. G. Wells (1866-1946)",
+                "The whole problem with the world is that fools and fanatics are always so certain of themselves, and wiser people so full of doubts.- Bertrand Russell (1872-1970)",
+                "Victory goes to the player who makes the next-to-last mistake. Savielly Grigorievitch Tartakower (1887-1956)",
+                "Don't be so humble - you are not that great. - Golda Meir (1898-1978) ",
+                "You can avoid reality, but you cannot avoid the consequences of avoiding reality. - Ayn Rand (1905-1982)"};
+
             InitializeComponent();
+        }
+
+        public void RotateQuote()
+        {
+            if (tracker == quotes.Length - 1)
+            {
+                tracker = 0;
+            }
+            else
+            {
+                tracker += 1;
+            }
+
+            txtDisplay.Text = quotes[tracker];
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            btnNext.Text = "Quote " + (tracker + 1).ToString();
+            RotateQuote();            
         }
     }
 }
